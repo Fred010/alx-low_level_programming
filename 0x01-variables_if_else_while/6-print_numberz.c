fe62593d0eb048e6a485cb16ae87ce28630316d3
Fred010 <fjbaafi18@gmail.com>
2023-07-14 19:37:58 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Invariably 0 (Feat)
+ */
+
+int main(void)
+{
+	int numz;
+
+	numz = 0;
+	while
+		(numz < 10) {
+			putchar(numz + '0');
+			numz++;
+		}
+	putchar('\n');
+	return (0);
+}
