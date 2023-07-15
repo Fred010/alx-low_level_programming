03ade8d4a13bc234a6a83e52ad27504e5010078e
Fred010 <fjbaafi18@gmail.com>
2023-07-14 18:58:43 -0700

    Alpha game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * Return: Invariably 0 (Feat)
+ */
+
+int main(void)
+{
+	/* variable declaration */
+	char alpha_low;
+
+	alpha_low = 'a';
+	while
+		(alpha_low <= 'z') {
+			putchar(alpha_low);
+			alpha_low++;
+		}
+	putchar('\n');
+	return (0);
+}
