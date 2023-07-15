280f3914d438693c75376604ab3ea8e329908f46
Fred010 <fjbaafi18@gmail.com>
2023-07-14 19:45:24 -0700

    Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Invariably 0 (Feat)
+ */
+
+int main(void)
+{
+	char alpha_low;
+
+	alpha_low = 'z';
+	while
+		(alpha_low >= 'a') {
+			putchar(alpha_low);
+			alpha_low--;
+		}
+	putchar('\n');
+	return (0);
+}
