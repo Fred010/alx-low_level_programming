4e01e0a0e8168a84009f85727e67ff27143c2c5e
Fred010 <fjbaafi18@gmail.com>
2023-07-26 19:11:47 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: number of bytes from src to dest
+ * Return: (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int id = 0;
+	int len_dest = 0;
+
+	while (dest[id++])
+		len_dest++;
+	for (id = 0; src[id] && id < n; id++)
+		dest[len_dest] = src[id];
+	return (dest);
+}
