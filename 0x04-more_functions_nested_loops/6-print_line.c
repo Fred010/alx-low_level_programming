9ace1b01ba268baac3456363c6b394d9ef0b6048
Fred010 <fjbaafi18@gmail.com>
2023-07-29 17:14:00 -0700

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,18 @@
 
 void print_line(int n)
 {
+	int l;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar('_');
-
+		for (l = 0; l < n; l++)
+		{
+			_putchar(95);
+		}
 		_putchar('\n');
 	}
 }
