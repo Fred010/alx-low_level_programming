54466de59b0ec8273697045351901369b82fde57
Fred010 <fjbaafi18@gmail.com>
2023-07-31 20:13:51 -0700

    diagonal duty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,15 +11,20 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, mark;
-	int initial_sum = 0;
-	int final_sum = 0;
+	int intial_sum, final_sum, i;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
-		initial_sum = initial_sum + a[i];
-	for (mark = size - 1; mark <= (size * size) - size; mark = mark + size - 1)
-		final_sum = final_sum + a[mark];
+	intial_sum = 0;
+	final_sum = 0;
 
-	printf("Sum of primary diagonal: %d\n", initial_sum);
-	printf("Sum of secondary diagonal: %d\n", final_sum);
+	for (i = 0; i < size; i++)
+	{
+		intial_sum = intial_sum + a[i * size + i];
+	}
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		final_sum += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", intial_sum, final_sum);
 }
