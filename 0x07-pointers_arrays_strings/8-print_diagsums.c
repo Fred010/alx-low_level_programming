f7756bd692ff92ce8b18ea59d78b8744415d0ea8
Fred010 <fjbaafi18@gmail.com>
2023-07-31 19:47:09 -0700

    diagonal duty

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: integer value
+ * @size: size of square matrix of integers
+ * Return: Always (0)
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int initial_sum = 0;
+	int final_sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		initial_sum += a[i * size + i];
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		final_sum += a[i * size + (size - 1 - i)];
+	}
+
+	_putchar(initial_sum);
+	_putchar(final_sum);
+}
