f707eb8316096e38f8eb00fbf1c88d59e1883bc1
Fred010 <fjbaafi18@gmail.com>
2023-08-10 08:52:32 -0700

    trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: structure variable
+ *
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+# C - More malloc, free
+We will consider static memeory allocation, using:
+* malloc() to allocate to a structure
+* free() to de-allocate
+
+_memory allocation happens at runtime_
