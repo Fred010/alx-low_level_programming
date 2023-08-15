bef600934bedda79e5b9cafcd7871fc7fe3d876f
Fred010 <fjbaafi18@gmail.com>
2023-08-14 20:44:53 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+# C - Structures, typedef
+
+It is a keyword use to give a _type_ a new name
+* give names to user defined data types as well
+* typedef is limited to giving symbolic names
+* to types only where as #define i used for aliases
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines dog struct
+ * @name: element 1
+ * @age: element 2
+ * @owner: element 3
+ *
+ * Description: long struct for more definitions
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
